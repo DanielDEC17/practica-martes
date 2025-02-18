1a11826166ea860fc338ffdfe1bd78c46d4bc634
DanielDEC17 <87341144+DanielDEC17@users.noreply.github.com>
2025-02-18 12:48:16 -0500

    Update portada.php

--- martes/api/secciones/portada.php
+++ martes/api/secciones/portada.php
@@ -1,11 +1,10 @@
 <?php
 /*****
-SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
+SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERÉS EN ESTA CATEGORÍA
 *****/
-
 $portada = [
-"titulo" => "Diario El Hocicón, "Pobre, pero Honrado"",
-"autor" => "XXXX",
-"resumen" => "El resumen Febrero 2025",
+    "titulo" => "Diario El Hocicón, 'Pobre, pero Honrado'",
+    "autor" => "Nombre del Autor", // Reemplaza "Nombre del Autor" con el autor real
+    "resumen" => "En este número especial de febrero de 2025, exploramos historias inspiradoras sobre personas que enfrentan desafíos con dignidad y honestidad.", // Reemplaza con un resumen real
 ];
 ?>
