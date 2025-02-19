2cca29986d5d46f02f9cd254bb6601069a8735d5
DanielDEC17 <87341144+DanielDEC17@users.noreply.github.com>
2025-02-19 02:24:16 -0500

    Update economia.php

--- martes/api/secciones/economia.php
+++ martes/api/secciones/economia.php
@@ -2,10 +2,9 @@
 /*****
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
-
 $economia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Inflación Baja a su Nivel Más Bajo en 5 Años",
+    "autor" => "Economista Jefe del Banco Central",
+    "resumen" => "La inflación registró una caída significativa gracias a las políticas monetarias implementadas recientemente."
 ];
 ?>
