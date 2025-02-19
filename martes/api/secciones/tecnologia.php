0a4ee60af638822054cd280f2799702e57aee782
DanielDEC17 <87341144+DanielDEC17@users.noreply.github.com>
2025-02-19 02:26:18 -0500

    Update tecnologia.php

--- martes/api/secciones/tecnologia.php
+++ martes/api/secciones/tecnologia.php
@@ -2,10 +2,9 @@
 /*****
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
-
 $tecnologia = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Lanzamiento del Nuevo Smartphone Foldable 2025",
+    "autor" => "Tech News Daily",
+    "resumen" => "La última innovación en smartphones plegables promete revolucionar la industria móvil."
 ];
 ?>
