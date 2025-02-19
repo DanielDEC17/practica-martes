037b42cb067ac71761624cf3ab601e8c7fdecaa8
DanielDEC17 <87341144+DanielDEC17@users.noreply.github.com>
2025-02-19 02:24:35 -0500

    Update internacional.php

--- martes/api/secciones/internacional.php
+++ martes/api/secciones/internacional.php
@@ -2,10 +2,9 @@
 /*****
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
-
 $internacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Cumbre Global sobre el Cambio Climático 2025",
+    "autor" => "Agencia Internacional de Noticias",
+    "resumen" => "Líderes mundiales se reúnen para discutir estrategias contra el cambio climático y reducir las emisiones globales de carbono."
 ];
 ?>
