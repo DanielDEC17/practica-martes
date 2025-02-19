9a28b46465ee24bfdea86fcc428310cdfa295dc2
DanielDEC17 <87341144+DanielDEC17@users.noreply.github.com>
2025-02-19 02:27:21 -0500

    Update television.php

--- martes/api/secciones/television.php
+++ martes/api/secciones/television.php
@@ -2,10 +2,9 @@
 /*****
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
-
 $television = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Estreno de la Nueva Serie de Ciencia Ficción 'Galaxia X'",
+    "autor" => "Canal Prime TV",
+    "resumen" => "Una serie épica que explora los confines del universo y los misterios de la humanidad."
 ];
 ?>
