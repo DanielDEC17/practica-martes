bbb13a3a2d50c4a2db932fbebccebd340db1711d
DanielDEC17 <87341144+DanielDEC17@users.noreply.github.com>
2025-02-19 02:25:02 -0500

    Update nacional.php

--- martes/api/secciones/nacional.php
+++ martes/api/secciones/nacional.php
@@ -2,10 +2,9 @@
 /*****
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
-
 $nacional = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Reforma Fiscal Aprueba en el Congreso",
+    "autor" => "Redacción Nacional",
+    "resumen" => "El gobierno anuncia una nueva reforma fiscal para impulsar la economía local y reducir la evasión de impuestos."
 ];
 ?>
