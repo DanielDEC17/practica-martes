5e1f3209f63059b8e6fe7cff4f9edefc1797a2f7
DanielDEC17 <87341144+DanielDEC17@users.noreply.github.com>
2025-02-18 21:59:01 -0500

    Update deportes.php

--- martes/api/secciones/deportes.php
+++ martes/api/secciones/deportes.php
@@ -2,10 +2,9 @@
 /*****
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
-
 $deportes = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Final de la Copa Mundial de Fútbol 2025",
+    "autor" => "ESPN Sports",
+    "resumen" => "El equipo nacional logra la victoria en una emocionante final frente a miles de fanáticos."
 ];
 ?>
