35dbc352b03e70ce2b2a3f631dd6b00879761511
DanielDEC17 <87341144+DanielDEC17@users.noreply.github.com>
2025-02-19 02:33:54 -0500

    Update sociedad.php

--- martes/api/secciones/sociedad.php
+++ martes/api/secciones/sociedad.php
@@ -2,10 +2,9 @@
 /*****
 SUSTITUYE LAS XXX POR UN VALOR DE UNA NOTICIA DE INTERES EN ESTA CATEGORIA
 *****/
-
 $sociedad = [
-"titulo" => "XXX",
-"autor" => "XXX",
-"resumen" => "XXX",
+    "titulo" => "Iniciativas Comunitarias para Reducir la Desigualdad Social",
+    "autor" => "ONG Equidad Global",
+    "resumen" => "Proyectos locales buscan mejorar las condiciones de vida en comunidades marginadas."
 ];
 ?>
